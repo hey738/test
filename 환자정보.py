14db037c076da676c70922a1f6c4b3eb43112024
hey738 <126555196+hey738@users.noreply.github.com>
2025-07-13 20:55:50 +0900

    Update 환자정보.py

--- 환자정보.py
+++ 환자정보.py
@@ -307,7 +307,7 @@ label_rate = (
     alt.Chart(monthly)
       .transform_filter(alt.datum.growth_rate != None)
       .mark_text(
-          dy=-30,              # 막대 꼭대기 위로 약간 띄움
+          dy=-10,              # 막대 꼭대기 위로 약간 띄움
           align='center',
           baseline='bottom',
           fontWeight='bold',
@@ -325,7 +325,7 @@ label_count = (
     alt.Chart(monthly)
       .transform_filter(alt.datum.growth_rate != None)
       .mark_text(
-          dy=-25,               # growth_rate 레이블 바로 아래
+          dy=0,               # growth_rate 레이블 바로 아래
           align='center',
           baseline='top',
           fontWeight='bold',
