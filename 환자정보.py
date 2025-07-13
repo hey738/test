5f402b562c75c502f03e4e3a63112c447ab09fce
hey738 <126555196+hey738@users.noreply.github.com>
2025-07-13 19:21:50 +0900

    Update 환자정보.py

--- 환자정보.py
+++ 환자정보.py
@@ -279,7 +279,7 @@ monthly = curr_monthly.merge(
 )
 monthly['growth_rate'] = (monthly['환자수'] - monthly['ly_환자수']) / monthly['ly_환자수']
 monthly['count_label'] = (
-    monthly['ly_환자수'].map(lambda x: f"{x:,}명") + "->\n" +
+    monthly['ly_환자수'].map(lambda x: f"{x:,}명") + "->\\n" +
     monthly['환자수'].map(lambda x: f"{x:,}명")
 )
 
@@ -307,7 +307,7 @@ label_rate = (
     alt.Chart(monthly)
       .transform_filter(alt.datum.growth_rate != None)
       .mark_text(
-          dy=-10,              # 막대 꼭대기 위로 약간 띄움
+          dy=-15,              # 막대 꼭대기 위로 약간 띄움
           align='center',
           baseline='bottom',
           fontWeight='bold'
@@ -324,10 +324,11 @@ label_count = (
     alt.Chart(monthly)
       .transform_filter(alt.datum.growth_rate != None)
       .mark_text(
-          dy=-10,               # growth_rate 레이블 바로 아래
+          dy=-15,               # growth_rate 레이블 바로 아래
           align='center',
           baseline='top',
-          fontWeight='bold'
+          fontWeight='bold',
+          linebreak='\\n'
       )
       .encode(
           x='yearmonth(진료일자):O',
